aee8e95e0a8e139aea0721817d3440823f991858
leclerced <whyitssoimportant@bk.ru>
2025-10-10 00:57:36 +0300

    fix

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,6 +1,6 @@
-#![allow(unused)] // игнорируем неиспользуемые переменные/методы и т.д.
+#![allow(unused)]
 
-use std::io; // обычная библиотека ввода/вывода
+use std::io; // библиотека ввода/вывода
 use colored::*; // цвет вывода текста в консоли toml - colored = "2.1"
 
 fn main() {
@@ -13,60 +13,64 @@ fn main() {
         io::stdin().read_line(&mut coef).expect("Ошибка при чтении строки");
 
         let nums_result: Vec<Result<f64, _>> = coef
-            .trim()
-            .split_whitespace()
-            .map(|x| x.parse::<f64>())
+            .trim().
+            split_whitespace()
+            .map(|x| x.parse())
             .collect();
+
+        if nums_result.len() < 3 {
+            println!("{}", "Необходимо ввести 3 коэффициента".red());
+            continue;
+        }
 
         if nums_result.iter().any(|x| x.is_err()) {
             println!("{}", "Один или несколько параметров не являются дробным числом -> f64".red());
-            continue; // запрашиваем ввод заново
+            continue;
         }
 
-        let nums: Vec<f64> = coef
-            .trim()
-            .split_whitespace()
-            .map(|x| x.parse().expect("Введите корректное число"))
+        let nums: Vec<f64> = nums_result
+            .into_iter()
+            .map(|x| x.unwrap())
             .collect();
 
-        if nums.len() == 3 && nums[0] != 0.0 {
-            let a = nums[0];
-            let b = nums[1];
-            let c = nums[2];
-
-            let d = b*b - 4.0*a*c;
-            if d >= 0.0 {
-                let t1 = (-b + d.sqrt()) / (2.0 * a);
-                let t2 = (-b - d.sqrt()) / (2.0 * a);
-
-                if t1 >= 0.0 {
-                    result.push(t1.sqrt());
-                    result.push(-t1.sqrt());
-                }
-
-                if t2 >= 0.0 && t2 != t1 {
-                    result.push(t2.sqrt());
-                    result.push(-t2.sqrt());
-                }
-                else {
-                    for i in result {
-                        println!("{}", format!("Корень: {i}").green());
-                    }
-                    break;
-                }
-            } 
-            else {
-                println!("{}", "Действительных корней нет".red());
-            }
-        } 
-        else if nums[0] == 0.0 {
-            println!("{}", "a - не может быть нулевым".red());
-        } 
-        else if nums[0] != 2.0 {
-            println!("{}", "один из параметров не конвертируется в дробное значение".red());
+        if nums[0] == 0.0 {
+            println!("{}", "Коэффициент a не может быть равен нулю".red());
+            continue;
         }
-        else {
-            println!("{}", "Введите 3 коэффициента".red());
+
+        let a = nums[0];
+        let b = nums[1];
+        let c = nums[2];
+
+        let discriminant = b * b - 4.0 * a * c;
+        if discriminant >= 0.0 {
+            let t1 = (-b + discriminant.sqrt()) / (2.0 * a);
+            let t2 = (-b - discriminant.sqrt()) / (2.0 * a);
+
+            if t1 >= 0.0 {
+                result.push(t1.sqrt());
+                result.push(-t1.sqrt());
+            }
+
+            if t2 >= 0.0 {
+                result.push(t2.sqrt());
+                result.push(-t2.sqrt());
+            }
+
+            if t1 != t2 && t2 >= 0.0 {
+                result.push(t2.sqrt());
+                result.push(-t2.sqrt());
+            }
+
+            for i in result {
+                if i == 0.0 || i == -0.0 {
+                    continue;
+                }
+                println!("{}", format!("Корень: {i}").green());
+            }
+            break;
+        } else {
+            println!("{}", "Действительных корней нет".red());
         }
     }
 }
