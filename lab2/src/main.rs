d210c47f74a215781fb6d1686a433e0790439118
leclerced <whyitssoimportant@bk.ru>
2025-10-11 10:52:26 +0300

    create folder for reports + fix lab2

--- lab2/src/main.rs
+++ lab2/src/main.rs
@@ -1,90 +1,99 @@
 #![allow(unused)]
 
-trait GeometricShape { // геометрическая фигура 
-    fn print_info(&self);
-    fn find_area(&self) -> f64; // нахождение площади фигуры
+trait GeometricalShape { // сюда закинуть print_info()
+    fn find_area(&self) -> f64;
 }
 
-// structs --point
-struct Rectangle { // прямугольник
+// rectangle --point
+struct Rectangle {
     name: String,
     width: f64,
     height: f64,
 }
-
-struct Square { // квадрат
-    name: String,
-    side: f64,
-}
-
-static PI: f64 = 3.141592;
-struct Circle { // круг
-    name: String,
-    radius: f64,
-}
-// structs --endpoint
-
-// impl --point
-impl GeometricShape for Rectangle { 
+impl GeometricalShape for Rectangle {
     fn find_area(&self) -> f64 {
         self.width * self.height
     }
-
+}
+impl Rectangle {
     fn print_info(&self) {
-        println!("Name: {}, Width: {}, Height: {}", self.name, self.width, self.height);
+        println!("Фигура: {}, Ширина: {}, Длина: {}, Площадь: {}",
+            self.name,
+            self.height,
+            self.width,
+            self.find_area()
+        );
     }
 }
-// impl Rectangle {
-//     fn print_info(&self) {
-//         println!("Name: {}, Width: {}, Height: {}", self.name, self.width, self.height);
-//     }
-// }
+// rectangle --endpoint
 
-impl GeometricShape for Square {
+// Square --point
+struct Square {
+    name: String,
+    side: f64,
+}
+impl GeometricalShape for Square {
     fn find_area(&self) -> f64 {
-        self.side * self.side
-    }
-
-    fn print_info(&self) {
-        println!("Name: {}, Side: {}", self.name, self.side);
+        self.side*self.side
     }
 }
-// impl Square {
-//     fn print_info(&self) {
-//         println!("Name: {}, Side: {}", self.name, self.side);
-//     }
-// }
+impl Square {
+    fn print_info(&self) {
+        println!("Фигура: {}, Сторона: {}, Площадь {}",
+            self.name,
+            self.side,
+            self.find_area()
+        );
+    }
+}
+// Square --endpoint
 
-impl GeometricShape for Circle { 
+// circle --point
+static PI: f64 = 3.141592;
+struct Circle {
+    name: String,
+    radius: f64,
+}
+impl GeometricalShape for Circle {
     fn find_area(&self) -> f64 {
         PI*self.radius*self.radius
     }
-
+}
+impl Circle {
     fn print_info(&self) {
-        println!("Name: {}, Radius: {}", self.name, self.radius);
+        println!("Фигура: {}, Радиус: {}, Площадь: {}", 
+            self.name, 
+            self.radius, 
+            self.find_area()
+        );
+    }   
+}
+// circle --endpoint
+
+enum Shape {
+    Rectangle(Rectangle),
+    Square(Square),
+    Circle(Circle),
+}
+impl Shape {
+    fn print_info(&self) {
+        match self {
+            Shape::Rectangle(r) => r.print_info(),
+            Shape::Square(s) => s.print_info(),
+            Shape::Circle(c) => c.print_info(),
+        }
     }
 }
-// impl Circle {
-//     fn print_info(&self) {
-//         println!("Name: {}, Radius: {}", self.name, self.radius);
-//     }
-// }
-
-// fn f_find_area<T: GeometricShape>(item: &T) {
-//     item.find_area();
-// }
-// impl --endpoint
 
 fn main() {
-    let shapes: Vec<Box<dyn GeometricShape>> = vec! [
-        Box::new(Rectangle { name: "Rectangle".to_string(), width: 11.0, height: 12.0 }),
-        Box::new(Square { name: "Square".to_string(), side: 64.0 }),
-        Box::new(Circle { name: "Circle".to_string(), radius: 5.0 }),
+    let shapes: Vec<Shape> = vec! [
+        Shape::Rectangle(Rectangle { name: "Прямоугольник".to_string(), width: 12.0, height: 3.9 }),
+        Shape::Square(Square {name: "Квадрат".to_string(), side: 11.8}),
+        Shape::Circle(Circle { name: "Круг".to_string(), radius: 4.3 })
     ];
 
     for shape in shapes {
         shape.print_info();
-        println!("Area: {}", shape.find_area());
         println!("-=-=-=-=-=-=-");
     }
 }
---
+++ lab2/target/debug/deps/lab2-134da4c8301557fa.d
@@ -0,0 +1,5 @@
+/Users/macbook/pikyap_lab/lab2/target/debug/deps/lab2-134da4c8301557fa.d: src/main.rs
+
+/Users/macbook/pikyap_lab/lab2/target/debug/deps/lab2-134da4c8301557fa: src/main.rs
+
+src/main.rs:
